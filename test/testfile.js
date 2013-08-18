b7e3c6bbf339d8644c288147835578eb803508f3
ajlopez <webmaster@ajlopez.com>
2013-08-18 11:08:39 -0300

    Additional test, testing two functions in module

--- test/testfile.js
+++ test/testfile.js
@@ -2,6 +2,8 @@
 var simpleunit = require('..'),
     path = require('path'),
     assert = require('assert');
+    
+// test module with one function
 
 var onefunction = require('./files/onefunction');
 
@@ -10,3 +12,15 @@ assert.equal(onefunction.counter, 0);
 simpleunit.test(path.resolve(__dirname, './files/onefunction.js'));
 
 assert.equal(onefunction.counter, 1);
+
+// test module with two functions
+
+var twofunctions = require('./files/twofunctions');
+
+assert.equal(twofunctions.counter1, 0);
+assert.equal(twofunctions.counter2, 0);
+
+simpleunit.test(path.resolve(__dirname, './files/twofunctions.js'));
+
+assert.equal(twofunctions.counter1, 1);
+assert.equal(twofunctions.counter2, 1);
