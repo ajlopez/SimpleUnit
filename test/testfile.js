c37309e14dbc4760285ffef6d7edeba889a6d407
ajlopez <webmaster@ajlopez.com>
2013-09-01 14:15:23 -0300

    test.async(), test.done()

--- test/testfile.js
+++ test/testfile.js
@@ -6,30 +6,37 @@ var simpleunit = require('..'),
 // test module with one function
 
 var onefunction = require('./files/onefunction');
+var twofunctions = require('./files/twofunctions');
 
 assert.equal(onefunction.counter, 0);
 
-simpleunit.test(path.resolve(__dirname, './files/onefunction.js'));
-
-assert.equal(onefunction.counter, 1);
+simpleunit.test(path.resolve(__dirname, './files/onefunction.js'), function (err, result) {
+    assert.equal(err, null);
+    assert.equal(onefunction.counter, 1);
+    step2();
+});
 
 // test module with two functions
 
-var twofunctions = require('./files/twofunctions');
+function step2() {
+    assert.equal(twofunctions.counter1, 0);
+    assert.equal(twofunctions.counter2, 0);
 
-assert.equal(twofunctions.counter1, 0);
-assert.equal(twofunctions.counter2, 0);
-
-simpleunit.test(path.resolve(__dirname, './files/twofunctions.js'));
-
-assert.equal(twofunctions.counter1, 1);
-assert.equal(twofunctions.counter2, 1);
+    simpleunit.test(path.resolve(__dirname, './files/twofunctions.js'), function (err, result) {
+        assert.equal(err, null);
+        assert.equal(twofunctions.counter1, 1);
+        assert.equal(twofunctions.counter2, 1);
+        step3();
+    });
+}
 
 // test modules in directory
 
-simpleunit.test(path.resolve(__dirname, './files'));
-
-assert.equal(onefunction.counter, 2);
-assert.equal(twofunctions.counter1, 2);
-assert.equal(twofunctions.counter2, 2);
-
+function step3() {
+    simpleunit.test(path.resolve(__dirname, './files'), function (err, result) {
+        assert.equal(err, null);
+        assert.equal(onefunction.counter, 2);
+        assert.equal(twofunctions.counter1, 2);
+        assert.equal(twofunctions.counter2, 2);
+    });
+}
